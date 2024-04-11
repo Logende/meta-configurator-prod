6f3a349d9e486f1b257babb9f34850d6598e7add
Felix Neubauer <felix@neuby.de>
2024-04-11 22:56:35 +0200

    make schema wrapper and class naming less ambiguous

--- meta_configurator/src/schema/oneTimeSchemaPreprocessor.ts
+++ meta_configurator/src/schema/oneTimeSchemaPreprocessor.ts
@@ -1,6 +1,6 @@
 import type {
   JsonSchemaObjectType,
-  JsonSchemaType,
+  JsonSchemaType, JsonSchemaTypePreprocessed,
   SchemaPropertyType,
 } from '@/model/jsonSchemaType';
 import _ from 'lodash';
@@ -17,8 +17,9 @@ import 'crypto';
  * - inject types of enum to types property
  *
  * @param schema the schema to preprocess
+ * @returns preprocessed schema
  */
-export function preprocessOneTime(schema: JsonSchemaType): JsonSchemaType {
+export function preprocessOneTime(schema: JsonSchemaType): JsonSchemaTypePreprocessed {
   if (typeof schema !== 'object') {
     return schema;
   }
